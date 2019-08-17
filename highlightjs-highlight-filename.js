eaa81c34ed2bec755560437f7a576011365d6e84
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2019-08-17 20:09:33 +0900

    Make hljs.initHighlightFilenameOnLoad()

---
+++ highlightjs-highlight-filename.js
@@ -0,0 +1,42 @@
+(function(w, d) {
+    'use strict';
+
+    if(w.hljs) {
+        w.hljs.initHighlightFilenameOnLoad = initHighlightFilenameOnLoad;
+        w.hljs.highlightFilenameCode = highlightFilenameCode;
+    }
+
+    function initHighlightFilenameOnLoad(options) {
+        function callHighlightFilenameCode() {
+            var codes = d.getElementsByClassName('hljs');
+            for(var i = 0; i < codes.length; ++i) {
+                highlightFilenameCode(codes[i], options[i]);
+            }
+        }
+
+        if(d.readyState !== 'loading') {
+            callHighlightFilenameCode();
+        }
+        else {
+            w.addEventListener('DOMContentLoaded', function() {
+                callHighlightFilenameCode();
+            });
+        }
+    }
+
+    function highlightFilenameCode(code, option) {
+        code.style.paddingTop = '2.2em';
+        code.style.position = 'relative';
+
+        var filename = d.createElement('div');
+        filename.style.position = 'absolute';
+        filename.style.top = '0em';
+        filename.style.left = '2em';
+        filename.style.backgroundColor = option.backgroundColor || '#888';
+        filename.style.color = option.color || 'white';
+        filename.style.padding = '0.2em';
+        filename.textContent = option.filename;
+
+        code.insertBefore(filename, code.firstChild);
+    }
+}(window, document));
